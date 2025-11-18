63dd51d94297e555fc2800daadbbc920b58c6fc4
Diego Eiras <eiras.lucio@gmail.com>
2025-11-18 15:38:12 -0300

    feat: discloud upload

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import 'dotenv/config'
 
-import { Client, LocalAuth } from 'whatsapp-web.js'
+import { Client } from 'whatsapp-web.js'
 
 import { connectToDatabase } from './config/db'
 
-const NODE_ENV = process.env.NODE_ENV || 'development'
-
 await connectToDatabase()
 
-export const client = new Client({
-	authStrategy: new LocalAuth(),
-	puppeteer: { headless: NODE_ENV === 'development' ? false : true },
-})
+export const client = new Client({})
