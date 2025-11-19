f7415cd5c59bf244cf9e93b03ea6a148cf8ad054
Diego Eiras <eiras.lucio@gmail.com>
2025-11-19 00:21:42 -0300

    fix: calculate stats after store fish

--- src/services/fishing.ts
+++ src/services/fishing.ts
@@ -1,5 +1,3 @@
-import type { Contact } from 'whatsapp-web.js'
-
 import { Baits } from '../models/baits'
 import { Fish } from '../models/fish'
 import { Rod } from '../models/rod'
@@ -20,43 +18,6 @@ export const fishing = async (user: UserType): Promise<string> => {
 
 		const userAfterFish = await User.findById(user._id)
 
-		const remainTimeToNextBait = await Baits.time(userAfterFish!)
-		const remainTimeToNextBaitFormatted =
-			formatRemainingTime(remainTimeToNextBait)
-
-		const totalStatus = await Fish.findTotal(userAfterFish!.fishesIds)
-
-		const stats = {
-			userTotal: totalStatus.userTotal,
-			total: totalStatus.total,
-			rarestFish: await Fish.findRarest(userAfterFish!.fishesIds),
-			heavierFish: await Fish.findHeavier(userAfterFish!.fishesIds),
-		}
-		const availableBaits = await Baits.available(userAfterFish!)
-
-		const replyMessage = {
-			fish: '',
-			rarity: '',
-			xp: '',
-
-			trash: '',
-
-			total: `> 🐟 Você já pescou ${stats.userTotal} de ${stats.total} peixes`,
-			rarestFish: stats.rarestFish
-				? `\n> 💎 Seu peixe mais raro é um(a) *${stats.rarestFish.name}* (${stats.rarestFish.rarity.category})`
-				: '',
-			heavierFish: stats.heavierFish
-				? `\n> 🏆 Seu peixe mais pesado é um(a) *${stats.heavierFish.name}* de *${stats.heavierFish.weight / 1000}kg*!`
-				: '',
-			remainXp: `\n> 👤 Faltam ${XP.next(userAfterFish!.rod, userAfterFish!.xp)} pontos de xp para o próximo nível`,
-			baits: `> 🐛 Você tem *${availableBaits}* iscas disponíveis`,
-			remainTimeToNextBait:
-				availableBaits < 5
-					? `\n> ⏳ Próxima isca em *${remainTimeToNextBaitFormatted}*`
-					: '',
-			levelUp: '',
-		}
-
 		if (fish) {
 			const xp = XP.calculate(
 				user.rod,
@@ -71,51 +32,103 @@ export const fishing = async (user: UserType): Promise<string> => {
 				fishesIds: [...userAfterFish!.fishesIds, fish.id],
 			})
 
+			const userAfterStore = await User.findById(user._id)
+
+			const remainTimeToNextBait = await Baits.time(userAfterStore!)
+			const remainTimeToNextBaitFormatted =
+				formatRemainingTime(remainTimeToNextBait)
+
+			const totalStatus = await Fish.findTotal(userAfterStore!.fishesIds)
+			const stats = {
+				userTotal: totalStatus.userTotal,
+				total: totalStatus.total,
+				rarestFish: await Fish.findRarest(userAfterStore!.fishesIds),
+				heavierFish: await Fish.findHeavier(userAfterStore!.fishesIds),
+			}
+			const availableBaits = await Baits.available(userAfterStore!)
+
+			const replyMessage = {
+				fish: `🐠 ${user.name} pescou um(a) *${fish.name}* de *${fish.weight / 1000}kg* com uma ${user.rod.name} ${user.rod.emoji}!`,
+				rarity: `\n\n⭐ Esse é um peixe *${fish.rarity.category}*`,
+				xp: `\n\n📈 Você ganhou *${xp}* pontos de xp!`,
+				remainXp: `\n\n> 👤 Faltam ${XP.next(userAfterStore!.rod, userAfterStore!.xp)} pontos de xp para o próximo nível`,
+				total: `\n> 🐟 Você já pescou ${stats.userTotal} de ${stats.total} peixes`,
+				rarestFish: stats.rarestFish
+					? `\n> 💎 Seu peixe mais raro é um(a) *${stats.rarestFish.name}* (${stats.rarestFish.rarity.category})`
+					: '',
+				heavierFish: stats.heavierFish
+					? `\n\n> 🏆 Seu peixe mais pesado é um(a) *${stats.heavierFish.name}* de *${stats.heavierFish.weight / 1000}kg*!`
+					: '',
+				baits: `> 🐛 Você tem *${availableBaits}* iscas disponíveis`,
+				remainTimeToNextBait:
+					availableBaits < 5
+						? `\n> ⏳ Próxima isca em *${remainTimeToNextBaitFormatted}*`
+						: '',
+				levelUp: '',
+			}
+
 			if (await XP.verify(user.rod, user.xp + xp)) {
 				const newRod = await Rod.findNext(userAfterFish!.rod)!
 
 				await XP.levelUp(userAfterFish!, newRod!)
 
 				replyMessage.levelUp = `\n\n🎉 Parabéns! Você subiu de nível e ganhou uma ${newRod!.name} ${newRod!.emoji}`
-				replyMessage.remainXp = ''
+				replyMessage.remainXp = `\n\n> 👤 Faltam ${newRod?.xpNext} pontos de xp para o próximo nível`
 			}
-
-			replyMessage.fish = `🐠 ${user.name} pescou um(a) *${fish.name}* de *${fish.weight / 1000}kg* com uma ${user.rod.name} ${user.rod.emoji}!`
-			replyMessage.rarity = `⭐ Esse é um peixe *${fish.rarity.category}*`
-			replyMessage.xp = `📈 Você ganhou *${xp}* pontos de xp!`
 
 			return (
 				replyMessage.fish +
-				'\n\n' +
 				replyMessage.rarity +
-				'\n\n' +
 				replyMessage.xp +
-				'\n' +
 				replyMessage.remainXp +
-				'\n' +
 				replyMessage.total +
 				replyMessage.rarestFish +
 				replyMessage.heavierFish +
-				'\n' +
 				replyMessage.baits +
 				replyMessage.remainTimeToNextBait +
 				replyMessage.levelUp
 			)
 		} else {
-			replyMessage.trash = `${user.name} pescou um(a) *${trash!.name}* ${trash!.emoji}. ${trash!.description}`
+			// Busca stats também quando pescar lixo
+			const remainTimeToNextBait = await Baits.time(userAfterFish!)
+			const remainTimeToNextBaitFormatted =
+				formatRemainingTime(remainTimeToNextBait)
+
+			const totalStatus = await Fish.findTotal(userAfterFish!.fishesIds)
+
+			const stats = {
+				userTotal: totalStatus.userTotal,
+				total: totalStatus.total,
+				rarestFish: await Fish.findRarest(userAfterFish!.fishesIds),
+				heavierFish: await Fish.findHeavier(userAfterFish!.fishesIds),
+			}
+			const availableBaits = await Baits.available(userAfterFish!)
+
+			const replyMessage = {
+				trash: `${user.name} pescou um(a) *${trash!.name}* ${trash!.emoji}. ${trash!.description}`,
+				remainXp: `\n\n> 👤 Faltam ${XP.next(userAfterFish!.rod, userAfterFish!.xp)} pontos de xp para o próximo nível`,
+				total: `\n> 🐟 Você já pescou ${stats.userTotal} de ${stats.total} peixes`,
+				rarestFish: stats.rarestFish
+					? `\n> 💎 Seu peixe mais raro é um(a) *${stats.rarestFish.name}* (${stats.rarestFish.rarity.category})`
+					: '',
+				heavierFish: stats.heavierFish
+					? `\n> 🏆 Seu peixe mais pesado é um(a) *${stats.heavierFish.name}* de *${stats.heavierFish.weight / 1000}kg*!`
+					: '',
+				baits: `\n> 🐛 Você tem *${availableBaits}* iscas disponíveis`,
+				remainTimeToNextBait:
+					availableBaits < 5
+						? `\n> ⏳ Próxima isca em *${remainTimeToNextBaitFormatted}*`
+						: '',
+			}
 
 			return (
 				replyMessage.trash +
-				'\n' +
 				replyMessage.remainXp +
-				'\n' +
 				replyMessage.total +
 				replyMessage.rarestFish +
 				replyMessage.heavierFish +
-				'\n' +
 				replyMessage.baits +
-				replyMessage.remainTimeToNextBait +
-				replyMessage.levelUp
+				replyMessage.remainTimeToNextBait
 			)
 		}
 	} else {
